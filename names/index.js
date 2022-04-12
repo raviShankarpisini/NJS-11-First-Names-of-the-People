fca2eeaffa885328473cc2179ee0bfeccaa3cbfe
ravishankarpisini <pisiniravishankar@gmail.com>
2022-04-12 10:15:06 +0000

    final submit

---
+++ names/index.js
@@ -0,0 +1,7 @@
+const peoplesNames = require("../country/state/city/index.js");
+const getFirstNames = require("../utilities/utils/index.js");
+
+const getPeopleInCity = (peoplesNames) => {
+  return getFirstNames(peoplesNames);
+};
+module.exports = getPeopleInCity;
